a94b7606c808e790531a2b540e7481ccbcd3bf70
smark1024 <kyehyun1024@gmail.com>
2025-05-09 16:51:01 +0900

    로직컬 연산자로 태그 유무 처리

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-export default function Profile({ image, name, title }) {
+export default function Profile({ image, name, title, badge }) {
     return (
         <div className="profile">
             <img className="photo" src={image} alt="avatar" />
+            {badge && <span className="badge">{badge}</span>}
             <h1>{name}</h1>
             <p>{title}</p>
         </div>
