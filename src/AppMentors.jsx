56d45ce3fdf49ee7e9242d254fa18ffc526d7f09
smark1024 <kyehyun1024@gmail.com>
2025-05-09 22:00:55 +0900

    복잡한 상태관리

--- src/AppMentors.jsx
+++ src/AppMentors.jsx
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 
 export default function AppMentor() {
-    const [person, setPerson] = useState({
-        name: "홍길동",
-        title: "개발자",
-        mentor: [
-            {
-                name: "임꺽정",
-                title: "시니어 개발자",
-            },
-            {
-                name: "가우스",
-                title: "시니어 개발자",
-            },
-        ],
-    });
+    const [person, setPerson] = useState(initialPerson);
+    const handleChange = () => {
+        const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
+        const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
+        setPerson({
+            ...person,
+            mentor: person.mentor.map((mentor) => {
+                if (mentor.name === prev) {
+                    return { ...mentor, name: current };
+                }
+                return mentor;
+            }),
+        });
+    };
+    const handleAdd = () => {
+        const name = prompt(`누구를 추가하고 싶은가요?`);
+        const title = prompt(`멘토의 타이틀은 무엇인가요?`);
+        setPerson({ ...person, mentor: [...person.mentor, { name, title }] });
+    };
+    const handleDelete = () => {
+        const name = prompt(`누구를 삭제하고 싶은가요?`);
+        setPerson({ ...person, mentor: person.mentor.filter((mentor) => mentor.name !== name) });
+    };
 
     return (
         <div>
@@ -30,23 +39,24 @@ export default function AppMentor() {
                 ))}
             </ul>
 
-            <button
-                onClick={() => {
-                    const prev = prompt(`누구의 이름을 바꾸고 싶은가요?`);
-                    const current = prompt(`이름을 무엇으로 바꾸고 싶은가요?`);
-                    setPerson({
-                        ...person,
-                        mentor: person.mentor.map((mentor) => {
-                            if (mentor.name === prev) {
-                                return { ...mentor, name: current };
-                            }
-                            return mentor;
-                        }),
-                    });
-                }}
-            >
-                멘토 이름 바꾸기
-            </button>
+            <button onClick={handleChange}>멘토 이름 바꾸기</button>
+            <button onClick={handleAdd}>멘토 추가하기</button>
+            <button onClick={handleDelete}>멘토 삭제하기</button>
         </div>
     );
 }
+
+const initialPerson = {
+    name: "홍길동",
+    title: "개발자",
+    mentor: [
+        {
+            name: "임꺽정",
+            title: "시니어 개발자",
+        },
+        {
+            name: "가우스",
+            title: "시니어 개발자",
+        },
+    ],
+};
