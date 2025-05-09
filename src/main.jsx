17be9a362cb8a353700ae9921fc5fbaa98899d71
smark1024 <kyehyun1024@gmail.com>
2025-05-09 21:33:17 +0900

    중첩 객세 상태 관리

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import AppXY from "./AppXY.jsx";
+import AppMentor from "./AppMentor.jsx";
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
-        <AppXY />
+        <AppMentor />
     </StrictMode>
 );
