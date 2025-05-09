ac588b879c363b64adc93d18e45f656d3954328a
smark1024 <kyehyun1024@gmail.com>
2025-05-09 15:26:52 +0900

    JS 이용하여 리스트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,16 @@ import "./App.css";
 
 function App() {
     const name = "John";
+    const list = ["사과", "바나나", "딸기"];
     return (
         <>
             <h1 className="orange">Hello</h1>
             <p>{name}</p>
+            <ul>
+                {list.map((item) => (
+                    <li>{item}</li>
+                ))}
+            </ul>
         </>
     );
 }
