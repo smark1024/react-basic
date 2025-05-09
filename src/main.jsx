721da8cab2faed42372128399bb761164e599a7f
smark1024 <kyehyun1024@gmail.com>
2025-05-09 16:31:51 +0900

    components에 props 사용

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import AppProfile from "./AppProfile.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <AppProfile />
+    </StrictMode>
+);
